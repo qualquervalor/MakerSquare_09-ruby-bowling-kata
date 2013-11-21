4eef031600aa258f3a0530879a6b42638350150c
Qualquer Valor <qualquervalor@gmail.com>
2013-11-21 11:46:31 -0600

    Updated rspec test for scoring a spare and updated bowling_game.score method to calculate spares

--- bowling_game.rb
+++ bowling_game.rb
@@ -14,10 +14,26 @@ class BowlingGame
 		@rolls.push(pins)
 	end
 
-# Returns the integeger score for this game. Expects to be run after all rolls
-# for the game have been recorded
+# Returns the Integer score for this game. Expects to be run after all rolls
+# for the game have been recorded.
 	def score
-		@rolls.reduce(:+)
+	  total_score  = 0
+	  current_roll = 0
+
+	  while current_roll < @rolls.size
+	    roll      = @rolls[current_roll]
+	    next_roll = @rolls[current_roll + 1]
+
+	    if roll + next_roll == 10
+	      total_score += 10 + @rolls[current_roll + 2]
+	    else
+	      total_score += roll + next_roll
+	    end
+
+	    current_roll += 2
+	  end
+
+	  return total_score
 	end
 
 
--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -4,7 +4,7 @@ require 'rspec'
 require_relative '../bowling_game'
 
 describe BowlingGame do 
-	let(:game){@game= BowlingGame.new}
+	let(:game){BowlingGame.new}
 	describe '.initialize' do
 		it 'sets @rolls to an empty array' do
 	
@@ -13,21 +13,29 @@ describe BowlingGame do
 	end
 
 	describe '#score' do
+		context 'when only gutter balls are thrown' do
+			it 'scores the game as 0' do
+				20.times { game.roll(0) }
 
-		it 'scores the game as 0 when only gutter balls are thrown' do
-			20.times { game.roll(0) }
-
-			expect(game.score).to eq(0)
+				expect(game.score).to eq(0)
+			end
 		end
 
-		it 'score the game as 20 when 1 pin is knocked down on each roll' do
-			20.times {game.roll(1)}
+		context 'when only one pin is knocked down on every roll' do
+			it 'score the game as 20' do
+				20.times {game.roll(1)}
 
-			expect(game.score).to eq(20)
+				expect(game.score).to eq(20)
+				end
+			end	
+
+		context 'when a spare is thrown' do
+			it 'records the frame score as 10 plus number of pins knocked down in next roll' do
+				3.times { game.roll(5) }
+				17.times { game.roll(0) }
+
+				expect(game.score).to eq (20)
 			end
+		end
 	end
-
-
-
-
 end
