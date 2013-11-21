d76663728e2db70f33dc8baf2c76b25c6ebc2e51
Qualquer Valor <qualquervalor@gmail.com>
2013-11-21 11:23:04 -0600

    Defined pins method and calculated score

--- bowling_game.rb
+++ bowling_game.rb
@@ -5,12 +5,19 @@ class BowlingGame
 		@rolls = []
 	end
 
+# Record a roll in the game.
+#
+# Pins - The Integer number of pins knocked down in this roll.
+#
+# Returns nothing
 	def roll(pins)
+		@rolls.push(pins)
 	end
 
-#Returns the integeger score for this game. Expects to be run after every roll
+# Returns the integeger score for this game. Expects to be run after all rolls
+# for the game have been recorded
 	def score
-		0
+		@rolls.reduce(:+)
 	end
 
 
